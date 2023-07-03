69b25dc1115aaebd6d7a777e6299eb3c172e4ac4
Srishti-garg <srishtigarg1632@gmail.com>
2023-07-03 20:32:32 +0530

    Initialize project using Create React App

---
+++ Backend/db.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+const mongoURI="mongodb://127.0.0.1:27017/inotebook";
+
+const connectToMongo=async ()=>{
+    try{
+        mongoose.set("strictQuery", false);
+        mongoose.connect(mongoURI,()=>{
+            console.log("connected to mongo successfully");
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+    
+};
+
+module.exports=connectToMongo;
